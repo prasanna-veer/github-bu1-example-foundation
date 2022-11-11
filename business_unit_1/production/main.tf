67ed3b79b544b396251a2224e24e81a2306e7964
Prasanna Veerabhadraiah <prasanna-auto-cft-org-admin-34%40org-admin-347813.iam.gserviceaccount.com>
2022-11-11 15:29:08 +0000

    storage demo

--- business_unit_1/production/main.tf
+++ business_unit_1/production/main.tf
@@ -5,7 +5,7 @@ locals {
 
 
 resource "google_storage_bucket" "bu1_demo" {
-  name          = "abd-bu1-c-infra-pipeline-2351-demo2"
+  name          = "abd-bu1-c-infra-pipeline-2351-demo3"
   location      = "europe-west2"
   project       = "abd-bu1-p-sample-base-8b3b"
   force_destroy = true
